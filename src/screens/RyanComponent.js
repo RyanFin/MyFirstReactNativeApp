6c18476bc1000400db74842fee3f5e3771df8f75
RyanFin <ryanfinlayson125@hotmail.com>
2024-08-02 14:43:30 +0100

    implement solution with stylesheet

--- src/screens/RyanComponent.js
+++ src/screens/RyanComponent.js
@@ -1,21 +1,21 @@
-import { Text } from "react-native";
+import { Text, StyleSheet } from "react-native";
 
 export default function RyanComponent() {
   const myName = "Ryan";
   return (
     <>
-      <Text style={{ fontSize: 45 }}>Getting started with React Native!</Text>
-      <Text style={{ fontSize: 20 }}>My name is {myName}.</Text>
+      <Text style={styles.headerText}>Getting started with React Native!</Text>
+      <Text style={styles.regularText}>My name is {myName}.</Text>
     </>
   );
-
-  const styles = StyleSheet.create({
-    headerText: {
-      fontSize: 45,
-    },
-
-    regularText: {
-      fontSize: 20,
-    },
-  });
 }
+
+const styles = StyleSheet.create({
+  headerText: {
+    fontSize: 45,
+  },
+
+  regularText: {
+    fontSize: 20,
+  },
+});
