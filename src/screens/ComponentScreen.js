1a2d378ca32b3f8b15081e01c75edf9a75772973
RyanFin <ryanfinlayson125@hotmail.com>
2024-08-02 14:25:20 +0100

    conditional rendering functions as well as React.js

--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -2,10 +2,15 @@ import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 
 export default function ComponentsScreen() {
+  var a = 1;
+  var b = 10;
+  var greeting = "Groot";
+
   return (
     <View>
       <Text style={styles.textStyle}> Hello World</Text>
-      <Text>Hi There!</Text>
+      <Text>{a < b ? "a is less than b" : "a is greater than b"}</Text>
+      {/* {greeting && <Text>{greeting}</Text>} */}
     </View>
   );
 }
