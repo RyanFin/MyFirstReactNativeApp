c59db269474e12dfaa6e2bfad43a5f94655facef
RyanFin <ryanfinlayson125@hotmail.com>
2024-08-02 21:18:39 +0100

    load images through props

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,8 +1,13 @@
-import { StyleSheet, Text } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function ImageDetail(props) {
   console.log(props);
-  return <Text> {props.title} </Text>;
+  return (
+    <View>
+      <Image source={props.imageName} />
+      <Text> {props.title} </Text>
+    </View>
+  );
 
   const styles = StyleSheet.create({});
 }
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -2,13 +2,18 @@ import { StyleSheet, Text, View } from "react-native";
 import ImageDetail from "../components/ImageDetail";
 
 export default function ImageScreen() {
+  const beach = require("../../assets/beach.jpg");
+  const forest = require("../../assets/forest.jpg");
+  const mountain = require("../../assets/mountain.jpg");
+  const volcano = require("../../assets/volcano.jpeg");
   return (
     // You can only return single root components in both React.js and React Native
+
     <View>
-      <ImageDetail title={"Forest"} imageName={""} />
-      <ImageDetail title={"Beach"} imageName={""} />
-      <ImageDetail title={"Lava"} imageName={""} />
-      <ImageDetail title={"Volcano"} imageName={""} />
+      <ImageDetail title={"Forest"} imageName={forest} />
+      <ImageDetail title={"Beach"} imageName={beach} />
+      <ImageDetail title={"Mountain"} imageName={mountain} />
+      <ImageDetail title={"Volcano"} imageName={volcano} />
     </View>
   );
 
