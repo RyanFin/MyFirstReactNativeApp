2b6bffb15ebfba700132a912f7c237df8260d547
RyanFin <ryanfinlayson125@hotmail.com>
2024-08-02 17:34:10 +0100

    add key to list

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,13 +3,18 @@ import { View, StyleSheet, Text, FlatList } from "react-native";
 
 export default function ListScreen() {
   const friends = [
-    { name: "Friend #1" },
-    { name: "Friend #2" },
-    { name: "Friend #3" },
-    { name: "Friend #4" },
+    { name: "Friend #1", key: "1" },
+    { name: "Friend #2", key: "2" },
+    { name: "Friend #3", key: "3" },
+    { name: "Friend #4", key: "4" },
+    { name: "Friend #5", key: "5" },
+    { name: "Friend #6", key: "6" },
+    { name: "Friend #7", key: "7" },
   ];
   return (
     <FlatList
+      //   keyExtractor={({ name }) => name}
+      keyExtractor={(friend) => friend.name}
       data={friends}
       renderItem={({ item, index }) => {
         return (
