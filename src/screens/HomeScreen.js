2d4e93feb932e766bbeb6a57f24537b6fbd4957e
RyanFin <ryanfinlayson125@hotmail.com>
2024-08-02 19:25:52 +0100

    refactoring

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,30 +3,31 @@ import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 import ComponentsScreen from "./ComponentScreen";
 import RyanComponent from "./RyanComponent";
 
-function HomeScreen(props) {
-  console.log(props.navigation);
-  console.log(props);
+// only retrieve the navigation propery from the entire props list.
+function HomeScreen({ navigation }) {
+  // console.log(props.navigation);
+  // console.log(props);
   return (
     <>
-      {/* <Text style={styles.text}>
-        Can run in iOS and Android at the same time
-      </Text> */}
-      {/* <ComponentsScreen /> */}
-      {/* <RyanComponent /> */}
-
       <Text style={styles.text}>Welcome to my first React Native App!</Text>
       {/* button is a self-closing element */}
+
       <Button
         title="Go to Components Demo"
         onPress={() => {
-          // console.log("button pressed.");
-          props.navigation.navigate("Components");
+          navigation.navigate("Components");
         }}
       />
+
+      <Button
+        title="Go to List demo"
+        onPress={() => navigation.navigate("List")}
+      />
+
       {/* not a self-closing element, unlike a button */}
-      <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
+      {/* <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
         <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </>
   );
 }
