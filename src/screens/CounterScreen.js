8bde55021214730dc0815a7331a2ed9dac5d5341
RyanFin <ryanfinlayson125@hotmail.com>
2024-08-03 15:40:35 +0100

    update code to use state instead of a variable

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,22 +1,27 @@
+import React, { useState } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 
 export default function CounterScreen() {
-  let counter = 0;
+  // array destructuring
+  const [counter, setCounter] = useState(0);
+  //   let counter = 0;
 
   return (
     <View>
       <Button
         title="Increase"
         onPress={() => {
-          counter += 1;
-          console.log(counter);
+          // counter += 1;
+          // console.log(counter);
+          setCounter(counter + 1);
         }}
       />
       <Button
         title="Decrease"
         onPress={() => {
-          counter -= 1;
-          console.log(counter);
+          //   counter -= 1;
+          //   console.log(counter);
+          setCounter(counter - 1);
         }}
       />
       <Text>Current Count: {counter}</Text>
