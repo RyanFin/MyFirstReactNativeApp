a649511d379194e9d22056b84f199747e36d1400
RyanFin <ryanfinlayson125@hotmail.com>
2024-08-06 18:40:12 +0100

    enter text and display

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function TextScreen() {
+  const [name, setName] = useState("");
   return (
     <View>
       <Text> Enter a name: </Text>
-      <TextInput style={styles.input} />
+      <TextInput
+        style={styles.input}
+        //   prevent iOS and Android from capitalising our characters
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={name}
+        onChangeText={(e) => {
+          setName(e);
+          console.log(name);
+        }}
+      />
+      <Text>My name is: {name}</Text>
     </View>
   );
 }
